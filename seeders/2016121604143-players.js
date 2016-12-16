31ae5c6d64aebb60dab0740868a3c3bc600aca6e
Brendan Kendrick <oneowhat@gmail.com>
2016-12-16 07:48:48 -0800

    unsaved change on player seed

--- seeders/2016121604143-players.js
+++ seeders/2016121604143-players.js
@@ -3,16 +3,7 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
     return queryInterface.bulkInsert('player', [
-      { name: "Alchemist's", active: true },
-      { name: "Brewer's", active: true },
-      { name: "Butcher's", active: true },
-      { name: "Engineer's", active: true },
-      { name: "Farmer's", active: false },
-      { name: "Fisherman's", active: true },
-      { name: "Hunter's", active: true },
-      { name: "Mason's", active: true },
-      { name: "Mortician's", active: true },
-      { name: "The Union", active: true }
+      { name: "Midas", guildId: 1, positionId: 1 },
     ], {});
   },
 
